5fd68607bc4671a180ecca7503a86560167659fc
Danielle <dwalraven21@gmail.com>
2022-03-09 17:59:09 -0500

    added items

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-function newImage (imgName, imgSrc, leftPx, bottomPx) {
-    imgName = document.createElement('img')
-    imgName.src = imgSrc
-    imgName.style.position = 'fixed'
-    imgName.style.left = leftPx
-    imgName.style.bottom = bottomPx
-    document.body.append(imgName)
+function newImage (imgSrc, leftPx, bottomPx) {
+    object = document.createElement('img')
+    object.src = imgSrc
+    object.style.position = 'fixed'
+    object.style.left = leftPx
+    object.style.bottom = bottomPx
+    document.body.append(object)
 }
 
-newImage('greenCharacter', 'assets/green-character.gif', '100px', '100px')
-newImage('pineTree', 'assets/pine-tree.png', '450px', '200px')
-newImage('pillar.', 'assets/pillar.png', '350px', '100px')
-newImage('crate', 'assets/crate.png', '150px', '200px')
-newImage('well', 'assets/well.png', '500px', '425px')
+function newItem (imgSrc, leftPx, bottomPx) {
+    object = document.createElement('img')
+    object.src = imgSrc
+    object.position = 'fixed'
+    object.left = leftPx
+    object.bottom = bottomPx
+    document.body.append(object)
+
+    object.addEventListener('dblclick', function(){
+        object.remove()
+    })
+}
+
+newImage('assets/green-character.gif', '100px', '100px')
+newImage('assets/pine-tree.png', '450px', '200px')
+newImage('assets/pillar.png', '350px', '100px')
+newImage('assets/crate.png', '150px', '200px')
+newImage('assets/well.png', '500px', '425px')
+
+newItem('assets/sword.png', '500px', '405px')
+newItem('assets/shield.png', '165px', '185px')
+newItem('assets/staff.png', '600px', '100px')
